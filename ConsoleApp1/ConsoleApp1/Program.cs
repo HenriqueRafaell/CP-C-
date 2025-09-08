58b602dcf04117525f4e4baad25e3a279d8b05d7
Henrique Rafael <143191316+HenriqueRafaell@users.noreply.github.com>
2025-09-08 08:46:25 -0300

    subindo arquivos cp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Variante Cálculo de média 
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o nome do estudante:");
+        string nome = Console.ReadLine();
+
+        double soma = 0;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.Write($"Digite a nota {i}: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            soma += nota;
+        }
+
+        double media = soma / 3;
+        Console.WriteLine($"\nA média do estudante {nome} é {media:F2}");
+    }
+}
